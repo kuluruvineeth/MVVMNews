e8f0146d5c70e8a0bb0b58a8597b3dfdebfca79f
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-06 18:37:05 +0530

    handled showing articles in webView.

--- app/src/main/java/com/kuluruvineeth/mvvmnews/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/kuluruvineeth/mvvmnews/ui/fragments/SavedNewsFragment.kt
@@ -3,15 +3,38 @@ package com.kuluruvineeth.mvvmnews.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kuluruvineeth.mvvmnews.R
+import com.kuluruvineeth.mvvmnews.adapters.NewsAdapter
 import com.kuluruvineeth.mvvmnews.ui.NewsActivity
 import com.kuluruvineeth.mvvmnews.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article",it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
