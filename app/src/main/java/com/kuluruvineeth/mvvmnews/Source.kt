a9359fd15bb743ebadf8f342759aaf3544712351
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-06 16:06:17 +0530

    completed adding all retrofit setups.

---
+++ app/src/main/java/com/kuluruvineeth/mvvmnews/Source.kt
@@ -0,0 +1,11 @@
+package com.kuluruvineeth.mvvmnews
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String
+)
---
+++ app/src/main/java/com/kuluruvineeth/mvvmnews/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.kuluruvineeth.mvvmnews.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "fde16ab00820440a899b507b1cdb0167"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
