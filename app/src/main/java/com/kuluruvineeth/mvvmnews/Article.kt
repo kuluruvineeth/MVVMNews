c1ba6df6cfa25a4c1d6b7d58e7fa9627a38207df
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-06 16:20:03 +0530

    created DAO and Entity for Article.

--- app/src/main/java/com/kuluruvineeth/mvvmnews/Article.kt
+++ app/src/main/java/com/kuluruvineeth/mvvmnews/Article.kt
@@ -1,9 +1,14 @@
 package com.kuluruvineeth.mvvmnews
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     @SerializedName("author")
     val author: String,
     @SerializedName("content")
